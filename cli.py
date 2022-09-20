0b527ff5ecca881774e3ea514f8b7883e7fc1d95
leo-stella <leo.hoshi3366@gmail.com>
2022-09-20 15:07:30 +0900

    fixed IF statements in resources.py changed error handling in cli.py

--- cli.py
+++ cli.py
@@ -5,13 +5,14 @@ import sys
 def get_capture_prob():
     capture_prob, mf_error, db_error, em_error = rs.get_data(sys.argv[1], sys.argv[2])
     if mf_error:
-        raise ValueError('There is something wrong with the "millennium-falcon.json" file')
+        return print('There is something wrong with the "millennium-falcon.json" file')
     elif db_error:
-        return ValueError('There is something wrong with the database')
+        return print('There is something wrong with the database')
     elif em_error:
-        return ValueError('There is something wrong with the "empire.json" file')
+        return print('There is something wrong with the "empire.json" file')
     else:
         return capture_prob
 
 
-print(get_capture_prob())
+get_capture_prob()
+
